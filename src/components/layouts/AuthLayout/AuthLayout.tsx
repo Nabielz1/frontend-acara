f0b650e16b73c16969237eb3c1c42074b64a3c74
Nabielz1 <gomblohsyukar@gmail.com>
2025-08-05 22:25:46 +0700

    feat: user activation

--- src/components/layouts/AuthLayout/AuthLayout.tsx
+++ src/components/layouts/AuthLayout/AuthLayout.tsx
@@ -1,20 +1,20 @@
 import PageHead from "@/components/commons/PageHead";
-import { ReactNode } from "react";
+import { Fragment, ReactNode } from "react";
 
 interface PropTypes {
-  title?: string;
   children: ReactNode;
+  title?: string;
 }
 
 const AuthLayout = (props: PropTypes) => {
   const { children, title } = props;
   return (
-    <div className="flex min-h-screen min-w-full flex-col items-center justify-center gap-10 py-10 lg:py-0">
+    <Fragment>
       <PageHead title={title} />
       <section className="max-w-screen-3xl 3xl:container p-6">
         {children}
       </section>
-    </div>
+    </Fragment>
   );
 };
 
